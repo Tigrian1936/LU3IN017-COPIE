5ca792657e59041d70d6a6c4e8273f3cbfda365b
Tigrian1936 <158016987+Tigrian1936@users.noreply.github.com>
2024-05-05 15:15:26 +0200

    micropush

--- app-react/src/Search.jsx
+++ app-react/src/Search.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 const SearchReturnType = {
-    THREAD: 0,
-    USER: 1,
-    MESSAGE: 2,
+    THREAD: "Thread",
+    USER: "User",
+    MESSAGE: "Message",
 };
 
 const SearchQueryType = {
-    TEXT: 0,
-    DATE: 1
+    TEXT: "Text",
+    DATE: "Date"
 }
 function Search(props) {
     const [returnType, setReturnType] = React.useState(SearchReturnType.THREAD);
@@ -20,7 +20,7 @@ function Search(props) {
     return (
         <div>
             <div>
-                <select onChange={evt => setReturnType(evt.value)}>
+                <select onChange={evt => setReturnType(evt.target.value)}>
                     <option value={SearchReturnType.THREAD}>Thread</option>
                     <option value={SearchReturnType.USER}>User</option>
                     <option value={SearchReturnType.MESSAGE}>Message</option>
@@ -30,16 +30,16 @@ function Search(props) {
                     {options.map((option, index) => {
                         return (
                             <div key={index}>
-                                <select onChange={evt => setOptions([...options.slice(0, index), { by : evt.value, type: option.type, value: null}, ...options.slice(index + 1, options.length)])}>
+                                <select onChange={evt => setOptions([...options.slice(0, index), { by : evt.target.value, type: option.type, value: null}, ...options.slice(index + 1, options.length)])}>
                                     <option value={SearchReturnType.MESSAGE}>Message</option>
                                     <option value={SearchReturnType.THREAD}>Thread</option>
                                     <option value={SearchReturnType.USER}>User</option>
                                 </select>
-                                <select onChange={evt => setOptions([...options.slice(0, index), {by : option.by, type: evt.value, value: null}, ...options.slice(index + 1, options.length)])}>
+                                <select onChange={evt => setOptions([...options.slice(0, index), {by : option.by, type: evt.target.value, value: null}, ...options.slice(index + 1, options.length)])}>
                                     <option value={SearchQueryType.TEXT}>Text</option>
                                     <option value={SearchQueryType.DATE}>Date</option>
                                 </select>
-                                <input type={options[index].type === SearchQueryType.DATE ? 'date':'text'} onChange={evt => setOptions([...options.slice(0, index), { type: options[index].value , value: evt.value}, ...options.slice(index + 1, options.length)])}></input>
+                                <input type={options[index].type === SearchQueryType.DATE ? 'date':'text'} onChange={evt => setOptions([...options.slice(0, index), { type: options[index].value , value: evt.target.value}, ...options.slice(index + 1, options.length)])}></input>
                             </div>
                         );
                     })}
