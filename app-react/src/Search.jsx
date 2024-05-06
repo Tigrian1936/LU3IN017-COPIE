a040ea5ebe858e38c89bfc219091cba622bc17d8
Tigrian1936 <158016987+Tigrian1936@users.noreply.github.com>
2024-05-06 17:08:39 +0200

    FEST

--- app-react/src/Search.jsx
+++ app-react/src/Search.jsx
@@ -42,14 +42,14 @@ function Search(props) {
                             {options[index].type === SearchQueryType.DATE ? 
                                 <input type="date" onChange={evt => 
                                     setOptions([...options.slice(0, index), 
-                                        { type: options[index].type , value: {from : evt.target.value, up_to : options[index].value.up_to}},
+                                        {by : option[index].by, type: options[index].type , value: {from : evt.target.value, up_to : options[index].value.up_to}},
                                         ...options.slice(index + 1, options.length)])}></input> : 
                                 <input type='text' onChange={evt => 
                                     setOptions([...options.slice(0, index), 
-                                        { type: options[index].type , value: evt.target.value}, 
+                                        {by : option[index].by, type: options[index].type , value: evt.target.value}, 
                                         ...options.slice(index + 1, options.length)])}></input>}
                             {options[index].type === SearchQueryType.DATE ? <label>To:</label> : null}
-                            {options[index].type === SearchQueryType.DATE ? <input type="date" onChange={evt => setOptions([...options.slice(0, index), { type: options[index].type , value: {from : options[index].value.from, up_to : evt.target.value}}, ...options.slice(index + 1, options.length)])}></input> : null}
+                            {options[index].type === SearchQueryType.DATE ? <input type="date" onChange={evt => setOptions([...options.slice(0, index), {by : option[index].by, type: options[index].type , value: {from : options[index].value.from, up_to : evt.target.value}}, ...options.slice(index + 1, options.length)])}></input> : null}
                             <button onClick={evt => setOptions([...options.slice(0, index), ...options.slice(index + 1, options.length)])}>Remove</button>
                         </div>
                     );
