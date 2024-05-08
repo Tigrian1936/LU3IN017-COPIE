55e04131204513dae8826bc2e5e00db0e1d1d1ee
Tigrian1936 <158016987+Tigrian1936@users.noreply.github.com>
2024-05-08 16:23:37 +0200

    aze

--- Server/src/index.js
+++ Server/src/index.js
@@ -7,7 +7,7 @@ const api = require('./api.js');
 app.use(express.json())
 app.use(cors({
     AccessControlAllowOrigin: '*',
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:5000/authentication/login',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
