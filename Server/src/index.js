f1a0aec7b86dc0779a66cfe40da9937d584abf1c
Tigrian1936 <158016987+Tigrian1936@users.noreply.github.com>
2024-05-08 17:35:22 +0200

    8080

--- Server/src/index.js
+++ Server/src/index.js
@@ -12,7 +12,7 @@ app.use(session({
 
 app.use(cors(
     {   
-        origin: 'http://localhost:8080',
+        origin: 'http://localhost:3000',
         credentials: true,
         methods : ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     }
