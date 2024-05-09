bf33803bbea36b128fb15c2ab23ec0f8ce23158d
Tigrian1936 <158016987+Tigrian1936@users.noreply.github.com>
2024-05-09 16:50:09 +0200

    aze

--- Server/src/index.js
+++ Server/src/index.js
@@ -16,12 +16,14 @@ app.use(session({
 app.use(cors({
     origin: 'http://localhost:5173',
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
 
 app.use((req, res, next) => {
     console.log(req.method, req.url);
     console.log(req.session);
     console.log(req.session.user);
+    console.log(req.session._ctx);
     next();
 });
 
